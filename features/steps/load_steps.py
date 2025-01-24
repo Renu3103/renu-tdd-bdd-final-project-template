eb0ab8b90af1d30a929aa5e14088df6b08ccd87b
Renu3103 <101053128+Renu3103@users.noreply.github.com>
2025-01-24 21:10:55 +0000

    Student_load_steps.py

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -44,9 +44,17 @@ def step_impl(context):
         assert(context.resp.status_code == HTTP_204_NO_CONTENT)
 
     #
-    # load the database with new products
+    # load the database with new studentinfo
     #
     for row in context.table:
         #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
+        for row in context.table:
+        payload = {
+            "id": row['id'],
+            "name": row['name'],
+            
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
+
         #
